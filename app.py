41a6c7fc2df7e20867892f4824b61776eb6d6fa9
Abdullah Akgül <akgula15@itu.edu.tr>
2019-11-18 16:24:13 +0300

    DB settings

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import dbinit
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 if __name__ == '__main__':
+    dbinit.db_init()
     app.run()
---
+++ dbinit.py
@@ -0,0 +1,45 @@
+import config
+import os
+import sys
+import psycopg2 as dbapi2
+
+INIT_STATEMENTS = [
+
+    """DROP TABLE IF EXISTS users cascade """,
+
+    """CREATE TABLE IF NOT EXISTS users 
+    (
+        user_id SERIAL NOT NULL PRIMARY KEY,
+        user_name VARCHAR(15) UNIQUE NOT NULL,
+        email VARCHAR (50) NOT NULL,
+        password VARCHAR (50) NOT NULL,
+        name VARCHAR (50) NOT NULL,
+        surname VARCHAR (50) NOT NULL,
+        phone VARCHAR (15) NOT NULL,
+        gender VARCHAR (1) NOT NULL,
+        address VARCHAR (250) NOT NULL,
+        last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        is_admin BOOLEAN NOT NULL DEFAULT FALSE
+    )"""
+
+
+]
+
+
+
+
+def initialize(url):
+    with dbapi2.connect(url) as connection:
+        cursor = connection.cursor()
+        for statement in INIT_STATEMENTS:
+            cursor.execute(statement)
+        cursor.close()
+
+
+def db_init():
+    url = config.DATABASE_URL
+    if url is None and config.DB_INIT_FLAG:
+        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
+        sys.exit(1)
+    initialize(url)
